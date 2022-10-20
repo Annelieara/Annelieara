b94f5dfd1eaa93a44c6e3dc0f1432843edb08477
Annelieara <anneliearajamanickam@gmail.com>
2022-10-20 15:18:48 +0530

    First Commit

---
+++ binarytodecimal.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n,b,d=0,base=1,r;
+    scanf("%d",&n);
+    b=n;
+    while(n>0)
+    {
+        r=n%10;
+        d=d+r*base;
+        n=n/10;
+        base=base*2
+        ;
+    }
+    printf("%d",d);
+}
